c6a48d57c625aa30e868c4890070474174d734ef
Weilai Xiang <xiangweilai99@gmail.com>
2018-02-07 20:25:58 +0800

    缩进

--- kruskal.cpp
+++ kruskal.cpp
@@ -10,11 +10,11 @@ const int maxm=10004;
 int n,m,f[maxn],k;
 struct edge
 {
-       int x,y,w;
+    int x,y,w;
 }s[maxm];
 bool cmp_edge(edge x,edge y)
 {
-     return x.w<y.w;
+    return x.w<y.w;
 }
 int find(int x)
 {
@@ -29,22 +29,22 @@ void kruskal()
 
     for(int i=1;i<=m;i++)
     {
-	int p=find(s[i].x);
-	int q=find(s[i].y);
-	if(q!=p)
-	{
-	    ans+=s[i].w;
-	    f[q]=p;
-	}
+        int p=find(s[i].x);
+        int q=find(s[i].y);
+        if(q!=p)
+        {
+            ans+=s[i].w;
+            f[q]=p;
+        }
     }
     bool flag=true; // is connected?
     for(int i=1;i<n;i++)
     {
-	if(find(i)!=find(i+1)) 
-	{
-	    flag=false;
+        if(find(i)!=find(i+1)) 
+        {
+            flag=false;
             break;
-	}
+        }
     }
     if(flag)printf("%d\n",ans); // min sum
     else printf("?\n"); // is not connected
