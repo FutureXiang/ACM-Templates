b9b8b3571ee10fb02a24be4756c369ceb36c7d44
XiangWeilai <xiangweilai99@gmail.com>
2019-09-13 12:37:56 +0800

    fixed bug in DIJKSTRA WITH PQ

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -6,8 +6,8 @@
 #include <cstdlib>
 #include <queue>
 using namespace std;
-const int maxn=30004;
-const int maxm=60004;
+const int maxn=100050;
+const int maxm=200050;
 struct Edge
 {
     int w;
@@ -18,6 +18,7 @@ struct vertex
 {
     Edge *next;
 }V[maxn];
+bool visit[maxn];
 int dis[maxn];
 int n,m,num;
 void addedge(int x,int y,int l)
@@ -34,6 +35,7 @@ void dijkstra(int S)
     priority_queue<P, vector<P>, greater<P> > q;
     q.push(P(0,S)); // pair greater: 按first第一，second第二。
 
+    memset(visit, 0, sizeof(visit));
     for(int i=1;i<=n;i++) dis[i]=INT_MAX;
     dis[S]=0;
     while(!q.empty())
@@ -41,21 +43,24 @@ void dijkstra(int S)
         P now=q.top();
         q.pop();
         int nowto=now.second,nowd=now.first;
-        if(dis[nowto]!=nowd) continue;
+        if (visit[nowto]) continue;
+        visit[nowto]=true;
         for(Edge *p=V[nowto].next;p;p=p->next)
         {
             int v=p->to, r=p->w+dis[nowto];
             if(dis[v]>r)
             {
                 dis[v]=r;
-                q.push(P(dis[v],v));
+                if (!visit[v])
+                    q.push(P(dis[v],v));
             }
         }
     }
 }
 int main()
 {
-    scanf("%d%d",&n,&m);
+    int S;
+    scanf("%d%d%d",&n,&m,&S);
     int u,v,l;
     for(int i=1;i<=n;i++) V[i].next=0;
     for(int i=1;i<=m;i++)
@@ -63,9 +68,9 @@ int main()
         scanf("%d%d%d",&u,&v,&l);
         addedge(u,v,l);
     }
-    dijkstra(1);
+    dijkstra(S);
     for(int i=1;i<=n;++i)
-        cout<<dis[i]<<endl;
-    getchar();getchar();
+        printf("%d ", dis[i]);
+    printf("\n");
     return 0;
 }
