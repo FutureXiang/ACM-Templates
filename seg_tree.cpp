1bc7dd2089752172ed7ea467c14eed768916b750
Xiang Weilai <33350017+FutureXiang@users.noreply.github.com>
2020-11-07 00:53:25 +0800

    [BUGFIX] 1. maxn*4 2. add mmax

--- seg_tree.cpp
+++ seg_tree.cpp
@@ -8,17 +8,20 @@ const int maxn = 200005;
 
 int n;
 struct seg {
-    long long w, lazy;
-    long long mmin;
-} s[maxn * 3];
+    long long sum, lazy;
+    long long mmin, mmax;
+} s[maxn * 4];
 
 void lazy_down(int now, int len) {
     s[now * 2].lazy += s[now].lazy;
     s[now * 2 + 1].lazy += s[now].lazy;
-    s[now * 2].w += (long long)(len - len / 2) * s[now].lazy;
-    s[now * 2 + 1].w += (long long)(len / 2) * s[now].lazy;
+
+    s[now * 2].sum += (long long) (len - len / 2) * s[now].lazy;
+    s[now * 2 + 1].sum += (long long) (len / 2) * s[now].lazy;
     s[now * 2].mmin += s[now].lazy;
     s[now * 2 + 1].mmin += s[now].lazy;
+    s[now * 2].mmax += s[now].lazy;
+    s[now * 2 + 1].mmax += s[now].lazy;
     s[now].lazy = 0;
 }
 
@@ -26,15 +29,17 @@ void build(int now, int l, int r) {
     if (l == r) {
         long long in;
         cin >> in;
-        s[now].w = in;
+        s[now].sum = in;
         s[now].mmin = in;
+        s[now].mmax = in;
         return;
     }
     int m = (l + r) / 2;
     build(now * 2, l, m);
     build(now * 2 + 1, m + 1, r);
-    s[now].w = s[now * 2].w + s[now * 2 + 1].w;
+    s[now].sum = s[now * 2].sum + s[now * 2 + 1].sum;
     s[now].mmin = min(s[now * 2].mmin, s[now * 2 + 1].mmin);
+    s[now].mmax = max(s[now * 2].mmax, s[now * 2 + 1].mmax);
 }
 
 void add(int now, int l, int r, int ql, int qr, long long A) {
@@ -42,8 +47,9 @@ void add(int now, int l, int r, int ql, int qr, long long A) {
     int m = (l + r) / 2;
     if (ql <= l && r <= qr) {
         s[now].lazy += A;
-        s[now].w += (long long)(A) * (long long)(r - l + 1);
+        s[now].sum += (long long) (A) * (long long) (r - l + 1);
         s[now].mmin += A;
+        s[now].mmax += A;
         return;
     }
 
@@ -51,15 +57,16 @@ void add(int now, int l, int r, int ql, int qr, long long A) {
 
     if (ql <= m) add(now * 2, l, m, ql, qr, A);
     if (qr > m) add(now * 2 + 1, m + 1, r, ql, qr, A);
-    s[now].w = s[now * 2].w + s[now * 2 + 1].w;
+    s[now].sum = s[now * 2].sum + s[now * 2 + 1].sum;
     s[now].mmin = min(s[now * 2].mmin, s[now * 2 + 1].mmin);
+    s[now].mmax = max(s[now * 2].mmax, s[now * 2 + 1].mmax);
 }
 
-long long query(int now, int l, int r, int ql, int qr, int flag) // flag = 0: sum;  flag = 1: min
+long long query(int now, int l, int r, int ql, int qr, int flag) // flag=0: sum   flag=1: min   flag=2: max
 {
     if (r < ql || l > qr) return 0;
-    long long ans = 0, mmin = LLONG_MAX;
-    if (ql <= l && r <= qr) return flag == 0 ? s[now].w : s[now].mmin;
+    long long ans = 0, mmin = LLONG_MAX, mmax = LLONG_MIN;
+    if (ql <= l && r <= qr) return flag == 0 ? s[now].sum : (flag == 1 ? s[now].mmin : s[now].mmax);
     int m = (l + r) / 2;
 
     if (s[now].lazy) lazy_down(now, r - l + 1);
@@ -70,14 +77,18 @@ long long query(int now, int l, int r, int ql, int qr, int flag) // flag = 0: su
     } else if (flag == 1) {
         if (ql <= m) mmin = min(mmin, query(now * 2, l, m, ql, qr, flag));
         if (qr > m) mmin = min(mmin, query(now * 2 + 1, m + 1, r, ql, qr, flag));
+    } else if (flag == 2) {
+        if (ql <= m) mmax = max(mmax, query(now * 2, l, m, ql, qr, flag));
+        if (qr > m) mmax = max(mmax, query(now * 2 + 1, m + 1, r, ql, qr, flag));
     }
-    return flag == 0 ? ans : mmin;
+    return flag == 0 ? ans : (flag == 1 ? mmin : mmax);
 }
 
 void init() {
     for (int i = 1; i <= n * 3; i++) {
-        s[i].w = s[i].lazy = 0;
+        s[i].sum = s[i].lazy = 0;
         s[i].mmin = LLONG_MAX; // +inf
+        s[i].mmax = LLONG_MIN; // -inf
     }
 }
 
@@ -92,11 +103,11 @@ int main() {
         int b, c;
         long long d;
         scanf("%d%d", &b, &c);
-        if (a == 'P') {
+        if (a == '+') {
             cin >> d;
             add(1, 1, n, b, c, d);
         } else {
-            cout << query(1, 1, n, b, c, a != 'S') << endl;
+            cout << query(1, 1, n, b, c, (a == '=' ? 0 : (a == '<' ? 1 : 2))) << endl;
         }
     }
     return 0;
